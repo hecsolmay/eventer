080defabc0c81f0d0c91a99e48ec7a9fa083306c
hecsolmay <hecsolmay@gmail.com>
2024-07-14 13:26:27 -0600

    add time utilities

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,5 @@
+import { DateValue, getLocalTimeZone, now, parseDate } from '@internationalized/date'
+
 export function formatEventDate (date: Date) {
   const dateString = date.toLocaleDateString('es-MX', {
     day: 'numeric',
@@ -7,4 +9,24 @@ export function formatEventDate (date: Date) {
   })
 
   return dateString
+}
+
+export function getParsedDateValue (date?: string | undefined | Date): DateValue {
+  try {
+    if (date instanceof Date) {
+      return parseDate(date.toISOString().split('T')[0])
+    }
+
+    return parseDate(date ?? new Date().toISOString().split('T')[0])
+  } catch (error) {
+    return parseDate(new Date().toISOString().split('T')[0])
+  }
+}
+
+export function DateValueToDate (date: DateValue): Date {
+  return date.toDate(getLocalTimeZone())
+}
+
+export function getNowDateValue (): DateValue {
+  return now(getLocalTimeZone())
 }
