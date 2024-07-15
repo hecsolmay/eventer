9fbd2d50c072ac10114a550b5383d28e2cc317fb
hecsolmay <hecsolmay@gmail.com>
2024-07-15 02:27:49 -0600

    extends getUserSession function and use public data

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,24 @@
 'use server'
 
+import prisma from './prisma'
 import { createClient } from './supabase/server'
 
-export async function getUserSession (){
+export async function getUserSession () {
   const supabase = createClient()
 
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
 
-  return user
+  if (user === null) {
+    return null
+  }
+
+  const userData = await prisma.users.findUnique({
+    where: {
+      id: user.id
+    }
+  })
+
+  return userData
 }
