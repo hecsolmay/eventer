dc49ebad3a251a4c77331c152f3609900284be81
hecsolmay <hecsolmay@gmail.com>
2024-07-22 00:54:59 -0600

    force dynamic on home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,8 @@
 import { ListOfRecentEvents } from '@/components/home/events'
 import Hero from '@/components/home/hero'
 
+export const dynamic = 'force-dynamic'
+
 export default function Home () {
   return (
     <>
